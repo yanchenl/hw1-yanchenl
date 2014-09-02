d75666f17690f525345762e39e5e1cb6519bff54
yanchenl <yanchenl@andrew.cmu.edu>
2014-09-02 11:49:11 -0400

    2nd commit

--- hw1-yanchenl/src/main/java/org/apache/uima/examples/XmiWriterCasConsumer.java
+++ hw1-yanchenl/src/main/java/org/apache/uima/examples/XmiWriterCasConsumer.java
@@ -20,21 +20,23 @@
 package org.apache.uima.examples;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.cas.FSIterator;
-import org.apache.uima.cas.impl.XmiCasSerializer;
+//import org.apache.uima.cas.impl.XmiCasSerializer;
 import org.apache.uima.collection.CasConsumer_ImplBase;
 import org.apache.uima.examples.SourceDocumentInformation;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.ResourceProcessException;
-import org.apache.uima.util.XMLSerializer;
+//import org.apache.uima.util.XMLSerializer;
 import org.xml.sax.SAXException;
 
 /**
@@ -98,7 +100,7 @@ public class XmiWriterCasConsumer extends CasConsumer_ImplBase {
         if (fileLoc.getOffsetInSource() > 0) {
           outFileName += ("_" + fileLoc.getOffsetInSource());
         }
-        outFileName += ".xmi";
+        outFileName += ".out";
         outFile = new File(mOutputDir, outFileName);
         modelFileName = mOutputDir.getAbsolutePath() + "/" + inFile.getName() + ".ecore";
       } catch (MalformedURLException e1) {
@@ -106,7 +108,7 @@ public class XmiWriterCasConsumer extends CasConsumer_ImplBase {
       }
     }
     if (outFile == null) {
-      outFile = new File(mOutputDir, "doc" + mDocNum++ + ".xmi");     
+      outFile = new File(mOutputDir, "doc" + mDocNum++ + ".out");
     }
     // serialize XCAS and write to output file
     try {
@@ -132,14 +134,19 @@ public class XmiWriterCasConsumer extends CasConsumer_ImplBase {
    */
   private void writeXmi(CAS aCas, File name, String modelFileName) throws IOException, SAXException {
     FileOutputStream out = null;
-
+    InputStream in = new FileInputStream(name);
+    out = new FileOutputStream(name);
     try {
-      // write XMI
-      out = new FileOutputStream(name);
-      XmiCasSerializer ser = new XmiCasSerializer(aCas.getTypeSystem());
-      XMLSerializer xmlSer = new XMLSerializer(out, false);
-      ser.serialize(aCas, xmlSer.getContentHandler());
+      byte[] buffer = new byte[1024];
+      while (true) {
+        int byteRead = in.read(buffer);
+        if (byteRead == -1)
+          break;
+        out.write(buffer, 0, byteRead);
+      }
     } finally {
+      if (in != null)
+        in.close();
       if (out != null) {
         out.close();
       }
